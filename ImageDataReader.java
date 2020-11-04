eb7025eeb21b840069975c148707e24fa1c78cfd
jcseok99 <jcseok99@gmail.com>
2020-11-04 16:55:29 +0900

    Add files via upload

---
+++ ImageDataReader.java
@@ -0,0 +1,21 @@
+package application;
+
+import data.ImageData;
+import java.io.IOException;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * Created by user on 2020-11-02.
+ */
+public class ImageDataReader {
+
+  public static ImageData readImageDataFile(String filePath) throws IOException {
+    //implement code
+    throw new NotImplementedException();
+  }
+
+  private static int readDataByLittleEndianToInt(byte[] readData) {
+    //implement code
+    throw new NotImplementedException();
+  }
+}
